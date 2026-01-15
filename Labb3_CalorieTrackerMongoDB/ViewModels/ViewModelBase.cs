faeaf1cdd5eb59df86e70391e595587c1729781d
SuthidaDamchum <suthida.damchum@hotmail.com>
2026-01-15 17:18:41 +0100

    Added ViewModelBase

---
+++ Labb3_CalorieTrackerMongoDB/ViewModels/ViewModelBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb3_CalorieTrackerMongoDB.ViewModels
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public event NotifyCollectionChangedEventHandler? CollectionChanged;
+        public void RaisePropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
+
+
