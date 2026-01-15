93fec7fcab973e3bbed6653a9db2260113ec7fbd
SuthidaDamchum <suthida.damchum@hotmail.com>
2026-01-15 17:17:33 +0100

    Added AsyndelegateCommand

---
+++ Labb3_CalorieTrackerMongoDB/Commands/AsyncDelegateCommand.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Labb3_CalorieTrackerMongoDB.Commands
+{
+    public class AsyncDelegateCommand : ICommand
+    {
+        private readonly Func<object?, Task> _executeAsync;
+        private readonly Func<object?, bool>? _canExecute;
+        private bool _isExecuting;
+
+        public AsyncDelegateCommand(Func<object?, Task> executeAsync, Func<object?, bool>? canExecute = null)
+        {
+            ArgumentNullException.ThrowIfNull(executeAsync);
+            _executeAsync = executeAsync;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object? parameter) =>
+            !_isExecuting && (_canExecute?.Invoke(parameter) ?? true);
+
+        public async void Execute(object? parameter)
+        {
+            if (!CanExecute(parameter)) return;
+            try
+            {
+                _isExecuting = true;
+                RaiseCanExecuteChanged();
+                await _executeAsync(parameter);
+            }
+            finally
+            {
+                _isExecuting = false;
+                RaiseCanExecuteChanged();
+            }
+        }
+
+        public event EventHandler? CanExecuteChanged;
+        public void RaiseCanExecuteChanged() =>
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+}
