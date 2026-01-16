6ee92cf263cd6ff2b9f1ca4ee5ba69973b6e3159
SuthidaDamchum <suthida.damchum@hotmail.com>
2026-01-16 12:31:17 +0100

    Added MainWindowViewModel to handle view switching

--- Labb3_CalorieTrackerMongoDB/ViewModels/MainWindowViewModel.cs
+++ Labb3_CalorieTrackerMongoDB/ViewModels/MainWindowViewModel.cs
@@ -1,19 +1,46 @@
-﻿using MongoDB.Driver;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Labb3_CalorieTrackerMongoDB.Commands;
+using Labb3_CalorieTrackerMongoDB.Models;
 
 namespace Labb3_CalorieTrackerMongoDB.ViewModels
 {
-
-    public class MainWindowViewModel
+    public class MainWindowViewModel : ViewModelBase
     {
-        private readonly MongoClient _client;
-        private readonly IMongoDatabase _database;
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get => _currentView;
+            set { _currentView = value; RaisePropertyChanged(); }
+        }
+
+
+        public DailyLogViewModel DailyLogVM { get; }
+        public FoodViewModel FoodVM { get; }
+        public WeeklySummaryViewModel WeeklySummaryVM { get; }
+
+      
+        public ICommand ShowTodaysLogCommand { get; }
+        public ICommand ShowFoodListCommand { get; }
+        public ICommand ShowWeeklySummaryCommand { get; }
 
         public MainWindowViewModel()
         {
-            var connectionString = "mongodb://localhost:27017/";
-            _client = new MongoClient(connectionString);
-            _database = _client.GetDatabase("SuthidaDamchum");
+            var mongoService = new MongoService();
+
+            DailyLogVM = new DailyLogViewModel(mongoService);
+            FoodVM = new FoodViewModel(DailyLogVM, mongoService);
+            WeeklySummaryVM = new WeeklySummaryViewModel(mongoService);
+
+            CurrentView = DailyLogVM; 
+
+            ShowTodaysLogCommand = new AsyncDelegateCommand(_ => { CurrentView = DailyLogVM; return Task.CompletedTask; });
+            ShowFoodListCommand = new AsyncDelegateCommand(_ => { CurrentView = FoodVM; return Task.CompletedTask; });
+            ShowWeeklySummaryCommand = new AsyncDelegateCommand(_ => { CurrentView = WeeklySummaryVM; return Task.CompletedTask; });
         }
 
+  
     }
 }
