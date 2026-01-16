d744e3622cec77cdd5c9726a279a3f71f35e5c1a
SuthidaDamchum <suthida.damchum@hotmail.com>
2026-01-16 12:17:49 +0100

    Added Add/Edit FoodList dialog

---
+++ Labb3_CalorieTrackerMongoDB/Dialogs/Add_Edit_FoodDailog.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Labb3_CalorieTrackerMongoDB.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for Add_Edit_FoodDailog..xaml
+    /// </summary>
+    public partial class Add_Edit_FoodDailog : Window
+    {
+        public Add_Edit_FoodDailog()
+        {
+            InitializeComponent();
+        }
+    }
+}
