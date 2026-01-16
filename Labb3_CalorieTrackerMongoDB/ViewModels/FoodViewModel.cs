53e33a0a386559f175d8cc4b9a4909a7bf3b6c50
SuthidaDamchum <suthida.damchum@hotmail.com>
2026-01-16 12:21:27 +0100

    Implemented Add/Edit Food dialog and hooked up Add, Update, Delete commands to buttons

--- Labb3_CalorieTrackerMongoDB/ViewModels/FoodViewModel.cs
+++ Labb3_CalorieTrackerMongoDB/ViewModels/FoodViewModel.cs
@@ -1,25 +1,65 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Labb3_CalorieTrackerMongoDB.Commands;
+using Labb3_CalorieTrackerMongoDB.Dialogs;
 using Labb3_CalorieTrackerMongoDB.Models;
 using MongoDB.Driver;
 
 namespace Labb3_CalorieTrackerMongoDB.ViewModels
 {
-    public class FoodViewModel
+    public class FoodViewModel : ViewModelBase
     {
+        private readonly MongoService _mongoService;
+        private readonly DailyLogViewModel _todaysLogVM;
 
-        static async Task AddFood(Food food)
+        public ObservableCollection<Food> Foods { get; set; }
+        public ICommand LoadFoodsCommand { get; }
+        public ICommand AddFoodCommand { get; }
+        public ICommand AddToTodayCommand { get; }
+        public ICommand EditFoodCommand { get; }
+
+        public FoodViewModel(DailyLogViewModel todaysLogVM, MongoService mongoService)
         {
-            var client = new MongoClient("mongodb://localhost:27017");
-            var database = client.GetDatabase("SuthidaDamchum");
-            var foodCollection = database.GetCollection<Food>("food");
+            _todaysLogVM = todaysLogVM;
+            _mongoService = mongoService;
 
-            await foodCollection.InsertOneAsync(food);
+            Foods = new ObservableCollection<Food>();
+
+            LoadFoodsCommand = new AsyncDelegateCommand(_ => LoadFoodsAsync());
+            AddFoodCommand = new AsyncDelegateCommand(_ => OpenFoodDialogAsync());
+            EditFoodCommand = new AsyncDelegateCommand(food => OpenFoodDialogAsync(food as Food));
+            AddToTodayCommand = new DelegateCommand(food =>
+            {
+                if (food is Food f)
+                    _todaysLogVM.TodaysFood.Add(f);
+            });
+
+            //load foods at startup
+            LoadFoodsCommand.Execute(null);
+        }
+
+        private async Task LoadFoodsAsync()
+        {
+            var foods = await _mongoService.Foods.Find(_ => true).ToListAsync();
+            Foods.Clear();
+            foreach (var food in foods)
+                Foods.Add(food);
         }
 
 
+        private async Task OpenFoodDialogAsync(Food? food = null)
+        {
+            var dialog = new Add_Edit_FoodDailog();
+            var vm = new FoodDialogViewModel(_mongoService, food);
+            dialog.DataContext = vm;
+
+            //show dialog 
+            dialog.ShowDialog();
+
+            //Reload foods after closing dialog
+            await LoadFoodsAsync();
+        }
     }
 }
