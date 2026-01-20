e67bf35519d1cd2f435353b7dfdc47722c3ff706
SuthidaDamchum <suthida.damchum@hotmail.com>
2026-01-20 15:56:00 +0100

    Implemented update and delete buttons

--- Labb3_CalorieTrackerMongoDB/ViewModels/FoodViewModel.cs
+++ Labb3_CalorieTrackerMongoDB/ViewModels/FoodViewModel.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 using Labb3_CalorieTrackerMongoDB.Commands;
 using Labb3_CalorieTrackerMongoDB.Dialogs;
 using Labb3_CalorieTrackerMongoDB.Models;
+using MongoDB.Bson;
 using MongoDB.Driver;
 
 namespace Labb3_CalorieTrackerMongoDB.ViewModels
@@ -12,13 +14,30 @@ namespace Labb3_CalorieTrackerMongoDB.ViewModels
     public class FoodViewModel : ViewModelBase
     {
         private readonly MongoService _mongoService;
-        private readonly DailyLogViewModel _todaysLogVM;
 
+ 
+
+
+
+        private readonly DailyLogViewModel _todaysLogVM;
         public ObservableCollection<Food> Foods { get; set; }
         public ICommand LoadFoodsCommand { get; }
         public ICommand AddFoodCommand { get; }
         public ICommand AddToTodayCommand { get; }
-        public ICommand EditFoodCommand { get; }
+        public ICommand DeleteCommand { get; }
+        public ICommand UpdateFoodCommand { get; }
+
+        private Food? _selectedFood;
+        public Food? SelectedFood
+        {
+            get => _selectedFood;
+            set
+            {
+                _selectedFood = value;
+                RaisePropertyChanged();
+                (DeleteCommand as AsyncDelegateCommand)?.RaiseCanExecuteChanged();
+            }
+        }
 
         public FoodViewModel(DailyLogViewModel todaysLogVM, MongoService mongoService)
         {
@@ -27,19 +46,30 @@ namespace Labb3_CalorieTrackerMongoDB.ViewModels
 
             Foods = new ObservableCollection<Food>();
 
+            DeleteCommand = new AsyncDelegateCommand(
+                 DeleteAsync,
+                 _ => SelectedFood != null
+             );
+
+
             LoadFoodsCommand = new AsyncDelegateCommand(_ => LoadFoodsAsync());
+
             AddFoodCommand = new AsyncDelegateCommand(_ => OpenFoodDialogAsync());
-            EditFoodCommand = new AsyncDelegateCommand(food => OpenFoodDialogAsync(food as Food));
+
+            UpdateFoodCommand = new AsyncDelegateCommand(
+                    food => OpenFoodDialogAsync(food as Food),
+                 _ => SelectedFood != null
+                );
+
             AddToTodayCommand = new DelegateCommand(food =>
             {
-                if (food is Food f)
+                if (food is Food f) 
                     _todaysLogVM.TodaysFood.Add(f);
             });
 
             //load foods at startup
             LoadFoodsCommand.Execute(null);
         }
-
         private async Task LoadFoodsAsync()
         {
             var foods = await _mongoService.Foods.Find(_ => true).ToListAsync();
@@ -48,18 +78,40 @@ namespace Labb3_CalorieTrackerMongoDB.ViewModels
                 Foods.Add(food);
         }
 
-
         private async Task OpenFoodDialogAsync(Food? food = null)
         {
             var dialog = new Add_Edit_FoodDailog();
             var vm = new FoodDialogViewModel(_mongoService, food);
             dialog.DataContext = vm;
 
-            //show dialog 
+
             dialog.ShowDialog();
 
-            //Reload foods after closing dialog
+
             await LoadFoodsAsync();
         }
+
+
+        private async Task DeleteAsync(object? obj)
+        {
+            if (SelectedFood == null)
+                return;
+
+            var result = MessageBox.Show($"Are you sure you want to delete {SelectedFood.Name} from the list?",
+                "Confirm Delete",
+                MessageBoxButton.YesNo,
+                MessageBoxImage.Warning);
+
+            if (result != MessageBoxResult.Yes)
+                return;
+
+            var filter = Builders<Food>.Filter.Eq(f => f.Id, SelectedFood.Id);
+            await _mongoService.Foods.DeleteOneAsync(filter);
+
+            Foods.Remove(SelectedFood);
+
+        }
+
+   
+        }
     }
-}
