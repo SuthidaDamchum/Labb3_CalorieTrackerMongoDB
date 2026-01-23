8ddcc1041702a69a304ad8e6ed1576c5c13dcb86
SuthidaDamchum <suthida.damchum@hotmail.com>
2026-01-23 14:39:11 +0100

    Fixed food list collection and replaced it with DailyItem

--- Labb3_CalorieTrackerMongoDB/ViewModels/DailyLogViewModel.cs
+++ Labb3_CalorieTrackerMongoDB/ViewModels/DailyLogViewModel.cs
@@ -1,49 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
+using System.Runtime.Versioning;
 using System.Windows.Input;
 using Labb3_CalorieTrackerMongoDB.Commands;
 using Labb3_CalorieTrackerMongoDB.Models;
 using MongoDB.Bson;
-using MongoDB.Bson.Serialization.Attributes;
 
 namespace Labb3_CalorieTrackerMongoDB.ViewModels
 {
     public class DailyLogViewModel : ViewModelBase
     {
-        public ObservableCollection<Food> TodaysFood { get; } = new ObservableCollection<Food>();
+        public ObservableCollection<DailyLogItem> TodaysItems { get; } = new();
 
         private readonly MongoService _mongoService;
-
- 
-        
         public ICommand SaveDayCommand { get; }
-
         public DateTime TodayDate => DateTime.Today;
         public int GoalCalories => 1500;
         public int GoalProtein => 100;
         public int GoalCarbs => 150;
         public int GoalFat => 50;
 
-        // Totals som uppdateras när du lägger till mat
-        public int TotalCalories => TodaysFood.Sum(f => f.Calories);
-        public int TotalProtein => TodaysFood.Sum(f => f.Protein);
-        public int TotalCarbs => TodaysFood.Sum(f => f.Carbs);
-        public int TotalFat => TodaysFood.Sum(f => f.Fat);
 
+        public int TotalCalories => TodaysItems.Sum(f => f.Calories);
+        public int TotalProtein => (int)Math.Round(TodaysItems.Sum(f => f.Protein));
+        public int TotalCarbs => (int)Math.Round(TodaysItems.Sum(f => f.Carbs));
+        public int TotalFat => (int)Math.Round(TodaysItems.Sum(f => f.Fat));
 
         public DailyLogViewModel(MongoService mongoService)
         {
             _mongoService = mongoService;
-            TodaysFood = new ObservableCollection<Food>();
 
-            SaveDayCommand = new AsyncDelegateCommand(_ => SaveDayAsync(), _=> TodaysFood.Any());
+            SaveDayCommand = new AsyncDelegateCommand(
+                _ => SaveDayAsync(), _ => TodaysItems.Any());
 
-            // Om du vill uppdatera totals när listan ändras
-            TodaysFood.CollectionChanged += (s, e) =>
+
+            TodaysItems.CollectionChanged += (s, e) =>
             {
                 RaisePropertyChanged(nameof(TotalCalories));
                 RaisePropertyChanged(nameof(TotalProtein));
@@ -51,26 +41,141 @@ namespace Labb3_CalorieTrackerMongoDB.ViewModels
                 RaisePropertyChanged(nameof(TotalFat));
             };
         }
-
         
-
         private async Task SaveDayAsync()
         {
+            if (!TodaysItems.Any())
+                return;
+
             var dailyLog = new DailyLog
             {
-
-                Date = TodayDate,
-                Foods = TodaysFood.ToList(),
+                Date = DateTime.Today,
+                Items = TodaysItems.ToList(),
                 TotalCalories = TotalCalories,
                 TotalProtein = TotalProtein,
                 TotalCarbs = TotalCarbs,
                 TotalFat = TotalFat
             };
 
-            await _mongoService.DailyLogs.InsertOneAsync(dailyLog);
+            await _mongoService.InsertDailyLogAsync(dailyLog);
+
+        }
+
+        public void AddFoodFromList(Food f, double ConsumedAmount)
+        {
+
+            var factor = ConsumedAmount / f.Amount;
+
+            var item = new DailyLogItem
+            {
+                FoodId = f.Id,
+                Name = f.Name,
+
+                Unit = f.Unit,
+                Amount = ConsumedAmount,
+                Calories = (int)Math.Round(factor * f.Calories),
+                Protein = factor * f.Protein,
+                Carbs = factor * f.Carbs,
+                Fat = factor * f.Fat,
+
+                Time = DateTime.Now
+            };
+
+            TodaysItems.Add(item);
+
+            // Update totals UI
+            RaisePropertyChanged(nameof(TotalCalories));
+            RaisePropertyChanged(nameof(TotalProtein));
+            RaisePropertyChanged(nameof(TotalCarbs));
+            RaisePropertyChanged(nameof(TotalFat));
+
+            // Enable Save button
+            (SaveDayCommand as AsyncDelegateCommand)?.RaiseCanExecuteChanged();
+        }
+
+        public async Task AddFoodToExistingDailyLogAsync(ObjectId dailyLogId, Food food, double amount)
+        {
+            var factor = amount / food.Amount;
+            var newItem = new DailyLogItem
+            {
+                FoodId = food.Id,
+                Name = food.Name,
+                Amount = amount,
+                Unit = food.Unit,
+                Time = DateTime.Now,
+                Calories = (int)Math.Round(factor * food.Calories),
+                Protein = factor * food.Protein,
+                Carbs = factor * food.Carbs,
+                Fat = factor * food.Fat
+            };
+
+            await _mongoService.AddItemToDailyLogAsync(dailyLogId, newItem);
         }
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
