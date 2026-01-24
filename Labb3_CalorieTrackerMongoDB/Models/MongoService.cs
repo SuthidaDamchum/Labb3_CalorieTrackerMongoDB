5a55ab490c140244ba4e3d6f137f33a5aa6da006
SuthidaDamchum <suthida.damchum@hotmail.com>
2026-01-24 22:48:33 +0100

    Added method UpdateDailyItem for increasing amount instead of add double food

--- Labb3_CalorieTrackerMongoDB/Models/MongoService.cs
+++ Labb3_CalorieTrackerMongoDB/Models/MongoService.cs
@@ -5,43 +5,35 @@ using MongoDB.Driver;
 
 namespace Labb3_CalorieTrackerMongoDB.Models
 {
-
     public class MongoService
     {
         private readonly IMongoDatabase _database;
-
         public MongoService()
         {
             var client = new MongoClient("mongodb://localhost:27017");
             _database = client.GetDatabase("SuthidaDamchum"); // Din databas
+           
         }
         public IMongoCollection<Food> Foods => _database.GetCollection<Food>("food");
         public IMongoCollection<DailyLog> DailyLogs => _database.GetCollection<DailyLog>("dailylogs");
-
-
 
         //public async Task InsertDailyLogAsync(DailyLog dailyLog)
         //{
         //    await DailyLogs.InsertOneAsync(dailyLog);
         //}
-
-
         public Task<DailyLog?> GetDailyLogByDateAsync(DateTime date)
         {
             var day = date.Date;
             return DailyLogs.Find(d => d.Date == day).FirstOrDefaultAsync();
         }
-
-
         public async Task<DailyLog> GetOrCreateTodayLogAsync(DateTime date)
         {
             var day = date.Date;
 
             var existing = await DailyLogs
                 .Find(d => d.Date == day)
+
                 .FirstOrDefaultAsync();
-
-
             if (existing != null)
                 return existing;
 
@@ -82,106 +74,25 @@ namespace Labb3_CalorieTrackerMongoDB.Models
             );
             await DailyLogs.UpdateOneAsync(filter, update);
         }
+        public async Task UpdateItemInDailyLogAsync(ObjectId dailyLogId, DailyLogItem item)
+        {
+            var filter = Builders<DailyLog>.Filter.And(
+                Builders<DailyLog>.Filter.Eq(log => log.Id, dailyLogId),
+                Builders<DailyLog>.Filter.ElemMatch(
+                    log => log.Items,
+                    i => i.FoodId == item.FoodId 
+                )
+            );
 
+            var update = Builders<DailyLog>.Update
+                .Set("Items.$.Amount", item.Amount)
+                .Set("Items.$.Calories", item.Calories)
+                .Set("Items.$.Protein", item.Protein)
+                .Set("Items.$.Carbs", item.Carbs)
+                .Set("Items.$.Fat", item.Fat)
+                .Set("Items.$.Time", item.Time);
 
-
+            await DailyLogs.UpdateOneAsync(filter, update);
+        }
     }
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
