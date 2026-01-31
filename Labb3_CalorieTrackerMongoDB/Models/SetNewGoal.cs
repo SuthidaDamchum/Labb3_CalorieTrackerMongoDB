d1d6a2124507db10f26cc1b1524df7805649a441
SuthidaDamchum <suthida.damchum@hotmail.com>
2026-01-31 23:42:44 +0100

    Added new model class set new goal

---
+++ Labb3_CalorieTrackerMongoDB/Models/SetNewGoal.cs
@@ -0,0 +1,11 @@
+﻿namespace Labb3_CalorieTrackerMongoDB.Models
+{
+    public class SetNewGoal
+    {
+        public int GoalCalories { get; set; }
+        public double GoalProtein { get; set; }
+        public double GoalCarbs { get; set; }
+        public double GoalFat { get; set; }
+    }
+}
+
