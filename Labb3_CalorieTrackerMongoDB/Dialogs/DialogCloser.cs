1c016c07f34365952791e4667a9fa93a4746e841
SuthidaDamchum <suthida.damchum@hotmail.com>
2026-01-31 23:34:45 +0100

    Added class dialogcloser

---
+++ Labb3_CalorieTrackerMongoDB/Dialogs/DialogCloser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Labb3_CalorieTrackerMongoDB.Dialogs
+{
+    class DialogCloser
+    {
+        public static readonly DependencyProperty DialogResultProperty =
+           DependencyProperty.RegisterAttached(
+               "DialogResult",
+               typeof(bool?),
+               typeof(DialogCloser),
+               new PropertyMetadata(null, OnDialogResultChanged));
+
+        public static void SetDialogResult(Window target, bool? value) =>
+            target.SetValue(DialogResultProperty, value);
+
+        public static bool? GetDialogResult(Window target) =>
+            (bool?)target.GetValue(DialogResultProperty);
+
+        private static void OnDialogResultChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is Window window && e.NewValue is bool result)
+            {
+                window.DialogResult = result;
+                window.Close();
+            }
+        }
+    }
+}
+    
