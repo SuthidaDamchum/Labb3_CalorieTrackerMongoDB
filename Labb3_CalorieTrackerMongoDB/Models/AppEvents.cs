40e4ace8afbc74bb87f1d1cf9143553c07f4447a
SuthidaDamchum <suthida.damchum@hotmail.com>
2026-01-31 23:45:07 +0100

    Added AppEvent

---
+++ Labb3_CalorieTrackerMongoDB/Models/AppEvents.cs
@@ -0,0 +1,16 @@
+﻿namespace Labb3_CalorieTrackerMongoDB.Models
+{
+    public static class AppEvents
+    {
+        public static event Func<Task>? DailyLogChanged;
+
+        public static async Task RaiseDailyLogChangedAsync()
+        {
+            var handlers = DailyLogChanged;
+            if (handlers == null) return;
+
+            foreach (Func<Task> handler in handlers.GetInvocationList())
+                await handler();
+        }
+    }
+}
