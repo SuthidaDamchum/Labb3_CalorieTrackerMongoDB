d571667b11c637e17292573d65074334a9d77c86
SuthidaDamchum <suthida.damchum@hotmail.com>
2026-01-31 23:37:06 +0100

    Changed unit ml to dl

--- Labb3_CalorieTrackerMongoDB/Models/Unit.cs
+++ Labb3_CalorieTrackerMongoDB/Models/Unit.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Labb3_CalorieTrackerMongoDB.Models
+﻿namespace Labb3_CalorieTrackerMongoDB.Models
 {
     public enum Unit
     {
         g,
-        ml,
+        dl,
         pcs
     }
 }
