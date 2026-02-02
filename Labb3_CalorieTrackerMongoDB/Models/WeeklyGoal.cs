56b72c18ed456512bffee34a55fbce5aa03c97ef
SuthidaDamchum <suthida.damchum@hotmail.com>
2026-02-02 18:04:28 +0100

    Added weekly goal class

---
+++ Labb3_CalorieTrackerMongoDB/Models/WeeklyGoal.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Labb3_CalorieTrackerMongoDB.Models
+{
+    public class WeeklyGoal
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+
+
+        [BsonDateTimeOptions(Kind = DateTimeKind.Utc)]
+        public DateTime WeekStart { get; set; }
+
+        public int GoalCalories { get; set; }
+        public double GoalProtein { get; set; }
+        public double GoalCarbs { get; set; }
+        public double GoalFat { get; set; }
+    }
+}
