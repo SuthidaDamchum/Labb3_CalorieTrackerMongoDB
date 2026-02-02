61c243073eb15c7424ef7dccb141474933969676
SuthidaDamchum <suthida.damchum@hotmail.com>
2026-02-02 18:05:13 +0100

    Reorganized folder

---
+++ Labb3_CalorieTrackerMongoDB/Services/DatabaseSeeder.cs
@@ -0,0 +1,126 @@
+﻿using System.Runtime.CompilerServices;
+using System.Windows.Media.Animation;
+using Labb3_CalorieTrackerMongoDB.Models;
+using MongoDB.Driver;
+
+
+namespace Labb3_CalorieTrackerMongoDB.Services
+
+{
+    public static class DatabaseSeeder
+    {
+        public static async Task SeedAsync(IMongoDatabase database)
+        {
+            await SeedFoodsAsync(database);
+            await SeedWeeklyGoalsAsync(database);
+            await SeedDailyLogsAsync(database);
+        }
+
+        private static async Task SeedFoodsAsync(IMongoDatabase database)
+        {
+            var foodCollection = database.GetCollection<Food>("food");
+
+            if (await foodCollection.Find(_ => true).AnyAsync())
+                return;
+
+            var foods = new List<Food>
+            {
+                new Food { Name = "Egg", Unit = Unit.pcs, Calories = 70, Protein = 6, Carbs = 1, Fat = 5 },
+                new Food { Name = "Chicken rice with salad", Unit = Unit.portion, Calories = 420, Protein = 40, Carbs = 45, Fat = 6 },
+                new Food { Name = "Apple", Unit = Unit.pcs, Calories = 95, Protein = 0, Carbs = 25, Fat = 0 },
+                new Food { Name = "Blueberry whey smoothie", Unit = Unit.portion, Calories = 215, Protein = 34, Carbs = 18, Fat = 2 },
+                new Food { Name = "Banana", Unit = Unit.pcs, Calories = 105, Protein = 1, Carbs = 27, Fat = 0 }
+            };
+
+            await foodCollection.InsertManyAsync(foods);
+        }
+
+        private static async Task SeedWeeklyGoalsAsync(IMongoDatabase database)
+        {
+            var collection = database.GetCollection<WeeklyGoal>("WeeklyGoals");
+
+            if (await collection.Find(_ => true).AnyAsync())
+                return;
+
+            var weeklyGoal = new WeeklyGoal
+            {
+                WeekStart = StartOfWeek(DateTime.Today, DayOfWeek.Monday),
+                GoalCalories = 1450,
+                GoalProtein = 105,
+                GoalCarbs = 130,
+                GoalFat = 47
+            };
+
+            await collection.InsertOneAsync(weeklyGoal);
+        }
+
+        private static async Task SeedDailyLogsAsync(IMongoDatabase database)
+        {
+            // MUST match MongoService collection name
+            var dailyLogs = database.GetCollection<DailyLog>("dailylogs");
+            var foods = database.GetCollection<Food>("food");
+            var weeklyGoals = database.GetCollection<WeeklyGoal>("WeeklyGoals");
+
+            if (await dailyLogs.Find(_ => true).AnyAsync())
+                return;
+
+            var goal = await weeklyGoals
+                .Find(_ => true)
+                .SortByDescending(g => g.WeekStart)
+                .FirstOrDefaultAsync();
+
+            var smoothie = await foods.Find(f => f.Name == "Blueberry whey smoothie").FirstOrDefaultAsync();
+            var chicken = await foods.Find(f => f.Name == "Chicken rice with salad").FirstOrDefaultAsync();
+            var apple = await foods.Find(f => f.Name == "Apple").FirstOrDefaultAsync();
+
+            var log = new DailyLog
+            {
+                Date = DateTime.UtcNow.Date, // keep it simple; consistent UTC date
+                GoalCalories = goal?.GoalCalories ?? 1450,
+                GoalProtein = (int)(goal?.GoalProtein ?? 105),
+                GoalCarbs = (int)(goal?.GoalCarbs ?? 130),
+                GoalFat = (int)(goal?.GoalFat ?? 47),
+                Items = new List<DailyLogItem>()
+            };
+
+            if (smoothie != null) log.Items.Add(CreateItemFromFood(smoothie));
+            if (chicken != null) log.Items.Add(CreateItemFromFood(chicken));
+            if (apple != null) log.Items.Add(CreateItemFromFood(apple));
+
+            await dailyLogs.InsertOneAsync(log);
+        }
+
+        private static DailyLogItem CreateItemFromFood(Food food)
+        {
+            return new DailyLogItem
+            {
+                FoodId = food.Id,
+                Name = food.Name,
+
+                Unit = food.Unit,
+                BaseUnit = food.Unit,
+                LogUnit = food.Unit,
+
+                BaseAmount = 1,
+                BaseCalories = food.Calories,
+                BaseProtein = food.Protein,
+                BaseCarbs = food.Carbs,
+                BaseFat = food.Fat,
+
+                Amount = 1,
+                Calories = food.Calories,
+                Protein = food.Protein,
+                Carbs = food.Carbs,
+                Fat = food.Fat,
+
+                Time = DateTime.Now
+            };
+        }
+
+        private static DateTime StartOfWeek(DateTime date, DayOfWeek startDay)
+        {
+            int diff = (7 + (date.DayOfWeek - startDay)) % 7;
+            return date.Date.AddDays(-diff);
+        }
+    }
+}
---
+++ Labb3_CalorieTrackerMongoDB/Services/MongoService.cs
@@ -0,0 +1,207 @@
+﻿using Labb3_CalorieTrackerMongoDB.Models;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace Labb3_CalorieTrackerMongoDB.Services
+{
+    public class MongoService
+    {
+        private readonly IMongoDatabase _database;
+        public MongoService()
+        {
+            var client = new MongoClient("mongodb://localhost:27017");
+            _database = client.GetDatabase("SuthidaDamchum"); 
+        }
+        public IMongoCollection<Food> Foods => _database.GetCollection<Food>("food");
+        public IMongoCollection<DailyLog> DailyLogs => _database.GetCollection<DailyLog>("dailylogs");
+        public IMongoCollection<WeeklyGoal> WeeklyGoals => _database.GetCollection<WeeklyGoal>("WeeklyGoals");
+
+        public DateTime GetWeekStartLocalMonday (DateTime localDate)
+        {
+            localDate = localDate.Date;
+
+            int diff = (7 + ((int)localDate.DayOfWeek - (int)DayOfWeek.Monday)) % 7;
+            return localDate.AddDays(-diff);
+        }
+
+        public async Task UpsertWeeklyGoalAsync (DateTime anyLocalDateInWeek, SetNewGoal goals)
+        {
+            var weekStartLocal = GetWeekStartLocalMonday(anyLocalDateInWeek);
+            var weekStartUtc = GetWeekStartUtcStockholm(weekStartLocal);
+
+            var filter = Builders<WeeklyGoal>.Filter.Eq(x => x.WeekStart, weekStartUtc);
+
+            var update = Builders<WeeklyGoal>.Update
+                .SetOnInsert(x => x.Id, ObjectId.GenerateNewId())
+                .Set(x => x.WeekStart, weekStartUtc)
+                .Set(x => x.GoalCalories, goals.GoalCalories)
+                .Set(x => x.GoalProtein, goals.GoalProtein)
+                .Set(x => x.GoalCarbs, goals.GoalCarbs)
+                .Set(x => x.GoalFat, goals.GoalFat);
+
+            await WeeklyGoals.UpdateOneAsync(filter, update, new UpdateOptions { IsUpsert = true });
+        }
+
+        public Task UpdateDailyGoalsForWeekAsync(DateTime anyLocalDateInWeek, SetNewGoal goals)
+        {
+            var weekStartLocal = GetWeekStartLocalMonday(anyLocalDateInWeek);
+            var startUtc = GetWeekStartUtcStockholm(weekStartLocal);
+            var endUtc = startUtc.AddDays(7);
+
+            var filter = Builders<DailyLog>.Filter.And(
+                Builders<DailyLog>.Filter.Gte(x => x.Date, startUtc),
+                Builders<DailyLog>.Filter.Lt(x => x.Date, endUtc)
+            );
+
+            var update = Builders<DailyLog>.Update
+                .Set(x => x.GoalCalories, goals.GoalCalories)
+                .Set(x => x.GoalProtein, (int)Math.Round(goals.GoalProtein))
+                .Set(x => x.GoalCarbs, (int)Math.Round(goals.GoalCarbs))
+                .Set(x => x.GoalFat, (int)Math.Round(goals.GoalFat));
+
+            return DailyLogs.UpdateManyAsync(filter, update);
+        }
+        public TimeZoneInfo StockholmTz => GetStockholmTimeZone();
+
+        public DateTime GetWeekStartUtcStockholm(DateTime weekStartLocalDate)
+        {
+
+            var tz = StockholmTz;
+            var unspecified = DateTime.SpecifyKind(weekStartLocalDate.Date, DateTimeKind.Unspecified);
+            return TimeZoneInfo.ConvertTimeToUtc(unspecified, tz);
+        }
+
+        private static TimeZoneInfo GetStockholmTimeZone()
+        {
+            try
+            {
+    
+                return TimeZoneInfo.FindSystemTimeZoneById("Europe/Stockholm");
+            }
+            catch (TimeZoneNotFoundException)
+            {
+          
+                return TimeZoneInfo.FindSystemTimeZoneById("W. Europe Standard Time");
+            }
+        }
+
+        private static DateTime ToUtcStartOfDayStockholm(DateTime localDate)
+        {
+            var tz = GetStockholmTimeZone();
+            var unspecified = DateTime.SpecifyKind(localDate.Date, DateTimeKind.Unspecified);
+            return TimeZoneInfo.ConvertTimeToUtc(unspecified, tz);
+        }
+
+        public Task UpdateDailyGoalsAyncs(DateTime localDate, int kcal, int p, int c, int f)
+        {
+            var startUtc = ToUtcStartOfDayStockholm(localDate);
+            var endUtc = startUtc.AddDays(1);
+
+            var filter = Builders<DailyLog>.Filter.And(
+                Builders<DailyLog>.Filter.Gte(x => x.Date, startUtc),
+                Builders<DailyLog>.Filter.Lt(x => x.Date, endUtc)
+            );
+
+            var update = Builders<DailyLog>.Update
+                .Set(x => x.GoalCalories, kcal)
+                .Set(x => x.GoalProtein, p)
+                .Set(x => x.GoalCarbs, c)
+                .Set(x => x.GoalFat, f);
+
+            return DailyLogs.UpdateOneAsync(filter, update);
+        }
+
+
+        public async Task<DailyLog?> GetDailyLogByDateAsync(DateTime localDate)
+        {
+            var startUtc = ToUtcStartOfDayStockholm(localDate);
+            var endUtc = startUtc.AddDays(1);
+
+            return await DailyLogs
+                .Find(x => x.Date >= startUtc && x.Date < endUtc)
+                .FirstOrDefaultAsync();
+        }
+
+
+
+        public Task AddItemToDailyLogAsync(ObjectId dailyLogId, DailyLogItem item)
+        {
+            var filter = Builders<DailyLog>.Filter.And(
+                Builders<DailyLog>.Filter.Eq(d => d.Id, dailyLogId),
+                Builders<DailyLog>.Filter.Not(
+                    Builders<DailyLog>.Filter.ElemMatch(d => d.Items, i => i.FoodId == item.FoodId)
+                )
+            );
+
+            var update = Builders<DailyLog>.Update.Push(d => d.Items, item);
+            return DailyLogs.UpdateOneAsync(filter, update);
+        }
+
+        public async Task RemoveItemFromDailyLogAsync(ObjectId dailyLogId, ObjectId foodID)
+        {
+            var filter = Builders<DailyLog>.Filter.Eq(log => log.Id, dailyLogId);
+            var update = Builders<DailyLog>.Update.PullFilter(
+                log => log.Items,
+                i => i.FoodId == foodID
+            );
+            await DailyLogs.UpdateOneAsync(filter, update);
+        }
+        public async Task UpdateItemInDailyLogAsync(ObjectId dailyLogId, DailyLogItem item)
+        {
+            var filter = Builders<DailyLog>.Filter.And(
+            Builders<DailyLog>.Filter.Eq(log => log.Id, dailyLogId),
+            Builders<DailyLog>.Filter.ElemMatch(log => log.Items, i => i.Id == item.Id)
+        );
+
+
+            var update = Builders<DailyLog>.Update
+                .Set("Items.$.Amount", item.Amount)
+                .Set("Items.$.Calories", item.Calories)
+                .Set("Items.$.Protein", item.Protein)
+                .Set("Items.$.Carbs", item.Carbs)
+                .Set("Items.$.Fat", item.Fat)
+                .Set("Items.$.Time", item.Time)
+                .Set("Items.$.LogUnit", item.LogUnit);
+
+            await DailyLogs.UpdateOneAsync(filter, update);
+        }
+
+        public async Task<DailyLog> GetOrCreateTodayLogAsync(DateTime localDate)
+        {
+            var startUtc = ToUtcStartOfDayStockholm(localDate);
+            var endUtc = startUtc.AddDays(1);
+
+            var existing = await DailyLogs
+                .Find(x => x.Date >= startUtc && x.Date < endUtc)
+                .FirstOrDefaultAsync();
+
+            if (existing != null)
+                return existing;
+
+            var weekStart = localDate.AddDays(-(int)localDate.DayOfWeek + (int)DayOfWeek.Monday).Date;
+            var weeklyGoal = await GetWeeklyGoalForDateAsync(localDate);
+
+            var newLog = new DailyLog
+            {
+                Date = startUtc,
+                Items = new List<DailyLogItem>(),
+                GoalCalories = weeklyGoal?.GoalCalories ?? 0,
+                GoalProtein = (int)(weeklyGoal?.GoalProtein ?? 0),
+                GoalCarbs = (int)(weeklyGoal?.GoalCarbs ?? 0),
+                GoalFat = (int)(weeklyGoal?.GoalFat ?? 0)
+            };
+
+            await DailyLogs.InsertOneAsync(newLog);
+            return newLog;
+        }
+
+        public async Task<WeeklyGoal?> GetWeeklyGoalForDateAsync(DateTime localDate)
+        {
+            var weekStartLocal = GetWeekStartLocalMonday(localDate);
+            var weekStartUtc = GetWeekStartUtcStockholm(weekStartLocal);
+
+            return await WeeklyGoals.Find(a => a.WeekStart == weekStartUtc).FirstOrDefaultAsync();
+        } 
+
+    }
+}
