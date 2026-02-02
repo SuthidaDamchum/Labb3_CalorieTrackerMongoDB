09da4c66ce56a779ca1c406d64f29ee679388e38
SuthidaDamchum <suthida.damchum@hotmail.com>
2026-02-02 18:12:55 +0100

    Added week navigation to summary view

--- Labb3_CalorieTrackerMongoDB/ViewModels/WeeklySummaryViewModel.cs
+++ Labb3_CalorieTrackerMongoDB/ViewModels/WeeklySummaryViewModel.cs
@@ -1,5 +1,8 @@
 ﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Labb3_CalorieTrackerMongoDB.Commands;
 using Labb3_CalorieTrackerMongoDB.Models;
+using Labb3_CalorieTrackerMongoDB.Services;
 using MongoDB.Driver;
 
 namespace Labb3_CalorieTrackerMongoDB.ViewModels
@@ -11,15 +14,14 @@ namespace Labb3_CalorieTrackerMongoDB.ViewModels
         public ObservableCollection<WeeklySummary> WeekItems { get; } = new();
 
 
-        private DateTime _start;
-        private DateTime _end;
+        private DateTime _weekStartLocal;
+        private DateTime _weekEndLocal;
 
-        private const int DbDayOffset = 1;
-
-        public string WeekRange => $"{_start:yyyy-MM-dd} → {_end:yyyy-MM-dd}";
+        public string WeekRange => $"{_weekStartLocal:yyyy-MM-dd} → {_weekEndLocal:yyyy-MM-dd}";
 
         public int DaysLogged => WeekItems.Count(x => x.HasLog);
         public int DaysOnTarget => WeekItems.Count(x => x.HasLog && x.ActualCalories <= x.GoalCalories);
+
 
         public double AvgCalories
         {
@@ -30,16 +32,58 @@ namespace Labb3_CalorieTrackerMongoDB.ViewModels
             }
         }
 
+        public int WeeklyGoalCalories => WeekItems.Sum(x => x.GoalCalories);
+        public int WeeklyActualCalories => WeekItems.Sum(x => x.ActualCalories);
+        public int WeeklyDiffCalories => WeeklyActualCalories - WeeklyGoalCalories;
+
+        public string WeekCaloriesSummaryText =>
+          DaysLogged == 0 ? "No logs yet" :
+          $"Total {WeeklyActualCalories} / Goal {WeeklyGoalCalories} (Diff {WeeklyDiffCalories:+#;-#;0})";
+
+        public ICommand PrevWeekCommand { get; }
+        public ICommand NextWeekCommand { get; }
+        public ICommand ThisWeekCommand { get; }
+
+
         public WeeklySummaryViewModel(MongoService mongoService)
         {
             _mongoService = mongoService;
 
+
+            _weekStartLocal = GetWeekStartLocalMonday(DateTime.Today);
+            _weekEndLocal = _weekStartLocal.AddDays(6);
+
+
             AppEvents.DailyLogChanged += OnDailyLogChangedAsync;
+
+            PrevWeekCommand = new AsyncDelegateCommand(async _ => await MoveWeekAsync(-7));
+            NextWeekCommand = new AsyncDelegateCommand(async _ => await MoveWeekAsync(+7));
+            ThisWeekCommand = new AsyncDelegateCommand(async _ => await GoToThisWeekAsync());
 
             _ = LoadAsync();
         }
 
-  
+        private static DateTime GetWeekStartLocalMonday(DateTime localDate)
+        {
+            localDate = localDate.Date;
+            int diff = (7 + ((int)localDate.DayOfWeek - (int)DayOfWeek.Monday)) % 7;
+            return localDate.AddDays(-diff);
+        }
+
+        private async Task MoveWeekAsync(int days)
+        {
+            _weekStartLocal = _weekStartLocal.AddDays(days);
+            _weekEndLocal = _weekStartLocal.AddDays(6);
+            await LoadAsync();
+        }
+
+        private async Task GoToThisWeekAsync()
+        {
+            _weekStartLocal = GetWeekStartLocalMonday(DateTime.Today);
+            _weekEndLocal = _weekStartLocal.AddDays(6);
+            await LoadAsync();
+        }
+
         private async Task OnDailyLogChangedAsync()
         {
             await LoadAsync();
@@ -47,40 +91,41 @@ namespace Labb3_CalorieTrackerMongoDB.ViewModels
 
         private async Task LoadAsync()
         {
-            var today = DateTime.Today;
-            int diff = ((int)today.DayOfWeek - (int)DayOfWeek.Monday + 7) % 7;
 
-            _start = today.AddDays(-diff).Date;
-            _end = _start.AddDays(6).Date;
-
-
-            var weekStartUtc = _mongoService.GetWeekStartUtcStockholm(_start); 
+            var weekStartUtc = _mongoService.GetWeekStartUtcStockholm(_weekStartLocal);
             var weekEndUtc = weekStartUtc.AddDays(7);
+
+            var wg = await _mongoService.GetWeeklyGoalForDateAsync(_weekStartLocal);
+
+            int goalKcal = wg?.GoalCalories ?? 0;
+            int goalP = (int)Math.Round(wg?.GoalProtein ?? 0);
+            int goalC = (int)Math.Round(wg?.GoalCarbs ?? 0);
+            int goalF = (int)Math.Round(wg?.GoalFat ?? 0);
 
             var logs = await _mongoService.DailyLogs
                 .Find(x => x.Date >= weekStartUtc && x.Date < weekEndUtc)
                 .SortBy(x => x.Date)
                 .ToListAsync();
 
-     
-            var byDate = logs.ToDictionary(
-                l => TimeZoneInfo.ConvertTimeFromUtc(DateTime.SpecifyKind(l.Date, DateTimeKind.Utc), _mongoService.StockholmTz).Date
+            var tz = _mongoService.StockholmTz;
+            var byLocalDate = logs.ToDictionary(
+                l => TimeZoneInfo.ConvertTimeFromUtc(DateTime.SpecifyKind(l.Date, DateTimeKind.Utc), tz).Date
             );
 
             WeekItems.Clear();
 
             for (int i = 0; i < 7; i++)
             {
-                var date = _start.AddDays(i).Date;
+                var date = _weekStartLocal.AddDays(i).Date;
 
-                if (byDate.TryGetValue(date, out var log))
+                if (byLocalDate.TryGetValue(date, out var log))
                 {
                     var items = log.Items ?? new();
 
                     WeekItems.Add(new WeeklySummary
                     {
                         Date = date,
-                        HasLog = true,
+                        HasLog = items.Count > 0,
 
                         GoalCalories = log.GoalCalories,
                         GoalProtein = log.GoalProtein,
@@ -99,10 +144,12 @@ namespace Labb3_CalorieTrackerMongoDB.ViewModels
                     {
                         Date = date,
                         HasLog = false,
-                        GoalCalories = 0,
-                        GoalProtein = 0,
-                        GoalCarbs = 0,
-                        GoalFat = 0,
+
+                        GoalCalories = goalKcal,
+                        GoalProtein = goalP,
+                        GoalCarbs = goalC,
+                        GoalFat = goalF,
+
                         ActualCalories = 0,
                         ActualProtein = 0,
                         ActualCarbs = 0,
@@ -115,17 +162,9 @@ namespace Labb3_CalorieTrackerMongoDB.ViewModels
             RaisePropertyChanged(nameof(DaysLogged));
             RaisePropertyChanged(nameof(DaysOnTarget));
             RaisePropertyChanged(nameof(AvgCalories));
-        }
-}
-
-
-        public static class AppEvents
-    {
-        public static event Func<Task>? DailyLogChanged;
-
-        public static void RaiseDailyLogChanged()
-        {
-            DailyLogChanged?.Invoke();
+            RaisePropertyChanged(nameof(WeekCaloriesSummaryText));
         }
     }
+
 }
+
--- Labb3_CalorieTrackerMongoDB/Views/WeeklySummaryView.xaml.cs
+++ Labb3_CalorieTrackerMongoDB/Views/WeeklySummaryView.xaml.cs
@@ -24,5 +24,7 @@ namespace Labb3_CalorieTrackerMongoDB.Views
         {
             InitializeComponent();
         }
+
+       
     }
 }
